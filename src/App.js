b9ea2319ae98b03b4f73ce891fdfe21546510535
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-12-07 19:57:08 +0700

    State of the forms

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import noteService from "./services/notes";
 import loginService from "./services/login";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
+import LoginForm from "./components/LoginForm";
 import Footer from "./components/Footer";
 import "./index.css";
+import Togglable from "./components/Togglable";
+import NoteForm from "./components/NoteForm";
 
 const App = () => {
+  // const [loginVisible, setLoginVisible] = useState(false);
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState("");
@@ -36,27 +39,10 @@ const App = () => {
     }
   }, []);
 
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
-    };
-
-    // axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-    //   setNotes(notes.concat(response.data));
-    //   setNewNote("");
-    // });
+  const addNote = (noteObject) => {
     noteService.create(noteObject).then((returnedNote) => {
       setNotes(notes.concat(returnedNote));
-      setNewNote("");
     });
-  };
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
@@ -115,35 +101,33 @@ const App = () => {
     }
   };
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
+  const loginForm = () => {
+    // const hideWhenVisible = { display: loginVisible ? "none" : "" };
+    // const showWhenVisible = { display: loginVisible ? "" : "none" };
+
+    return (
       <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
+        {/* <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>log in</button>
+        </div> */}
+        <Togglable buttonLabel="login">
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+          {/* <button onClick={() => setLoginVisible(false)}>cancel</button> */}
+        </Togglable>
       </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
+    );
+  };
 
   const noteForm = () => (
-    <form onSubmit={addNote}>
-      <input value={newNote} onChange={handleNoteChange} />
-      <button type="submit">save</button>
-    </form>
+    <Togglable buttonLabel="New Note">
+      <NoteForm createNote={addNote} />
+    </Togglable>
   );
 
   return (
---
+++ src/components/NoteForm.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const NoteForm = ({ createNote }) => {
+  const [newNote, setNewNote] = useState("");
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const addNote = (event) => {
+    event.preventDefault();
+    createNote({
+      content: newNote,
+      important: Math.random() > 0.5,
+    });
+
+    setNewNote("");
+
+    // axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+    //   setNotes(notes.concat(response.data));
+    //   setNewNote("");
+    // });
+  };
+
+  return (
+    <div>
+      <h2>Create a new note</h2>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default NoteForm;
